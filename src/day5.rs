a45cb672546ffb4eb57785e1cb49fc991d72e67b
Peter Thompson <peter@petethompson.net>
2023-12-08 16:49:54 +0100

    Add day 5 parsing

--- src/day5.rs
+++ src/day5.rs
@@ -1,7 +1,109 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use nom::bytes::complete::{is_a, is_not, tag};
+use nom::character::complete::{digit1, line_ending, space1};
+use nom::combinator::{all_consuming, map, map_res, opt};
+use nom::multi::separated_list1;
+use nom::sequence::{terminated, tuple};
+use nom::{IResult, Parser};
+use std::rc::Rc;
 
 pub fn part1(input: &str) -> Result<usize> {
+    let state = parse_lines::<State>(input, parse_state)?;
     Ok(1)
+}
+
+#[derive(Debug)]
+struct State {
+    seeds: Vec<usize>,
+    start_map: Rc<Map>,
+}
+
+#[derive(Debug)]
+struct Map {
+    name: String,
+    ranges: Vec<MapRange>,
+    next: Option<Rc<Map>>,
+}
+
+#[derive(Debug)]
+struct MapRange {
+    src_start: usize,
+    dst_start: usize,
+    size: usize,
+}
+
+fn number(digits: &str) -> IResult<&str, usize> {
+    map_res(digit1, |n: &str| n.parse::<usize>())(digits)
+}
+
+fn parse_seeds(input: &str) -> IResult<&str, Vec<usize>> {
+    let (input, _) = tag("seeds: ")(input)?;
+    separated_list1(space1, number)(input)
+}
+
+fn parse_map_range(input: &str) -> IResult<&str, MapRange> {
+    map(
+        tuple((
+            terminated(number, tag(" ")),
+            terminated(number, tag(" ")),
+            number,
+        )),
+        |(dst_start, src_start, size)| MapRange {
+            src_start,
+            dst_start,
+            size,
+        },
+    )(input)
+}
+
+fn parse_map(input: &str) -> IResult<&str, Map> {
+    let (input, name) = terminated(is_not(" "), tag(" map:"))(input)?;
+    let (input, _) = line_ending(input)?;
+    let (input, ranges) = separated_list1(line_ending, parse_map_range)(input)?;
+    Ok((
+        input,
+        Map {
+            name: name.to_owned(),
+            ranges,
+            next: None,
+        },
+    ))
+}
+
+fn parse_state(input: &str) -> IResult<&str, State> {
+    let (input, seeds) = parse_seeds(input)?;
+    let (input, _) = is_a("\r\n")(input)?;
+    let (input, maps) = separated_list1(is_a("\r\n"), parse_map)(input)?;
+
+    let mut start_map: Option<Rc<Map>> = None;
+    let mut prev_map: Option<Rc<Map>> = None;
+
+    for mut map in maps.into_iter().rev() {
+        if prev_map.is_some() {
+            map.next = Some(Rc::clone(&prev_map.take().unwrap()));
+        }
+        let map_rc = Rc::new(map);
+        start_map = Some(Rc::clone(&map_rc));
+        prev_map = Some(Rc::clone(&map_rc));
+    }
+
+    Ok((
+        input,
+        State {
+            seeds,
+            start_map: start_map.unwrap(),
+        },
+    ))
+}
+
+fn parse_lines<'a, T>(
+    input: &'a str,
+    parser: impl Parser<&'a str, T, nom::error::Error<&'a str>>,
+) -> Result<T> {
+    let (_, res) = all_consuming(terminated(parser, opt(line_ending)))(input)
+        .map_err(|e| e.to_owned())
+        .context("failed to parse input")?;
+    Ok(res)
 }
 
 #[cfg(test)]
