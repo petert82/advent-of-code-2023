e68a9e50f444cc91cef2aefcd45536e7325abe81
Peter Thompson <peter@petethompson.net>
2023-12-10 16:01:48 +0100

    Solve day 7 part 1

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Ordering, collections::HashMap};
+
 use anyhow::Result;
 use nom::{
     bytes::complete::tag,
@@ -11,12 +13,13 @@ use nom::{
 use crate::puzzle::parse_lines_to_vec;
 
 pub fn part1(input: &str) -> Result<usize> {
-    let hands = parse_lines_to_vec(input, parse_hand)?;
-    dbg!(hands);
-    Ok(1)
+    let mut hands = parse_lines_to_vec(input, parse_hand)?;
+    hands.sort();
+    let res = hands.iter().enumerate().map(|(i, h)| (i + 1) * h.bid).sum();
+    Ok(res)
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Card {
     Ace = 14,
     King = 13,
@@ -33,15 +36,92 @@ enum Card {
     Two = 2,
 }
 
-#[derive(Debug)]
-struct Hand {
-    cards: (Card, Card, Card, Card, Card),
-    bid: usize,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    FiveOfAKind = 7,
+    FourOfAKind = 6,
+    FullHouse = 5,
+    ThreeOfAKind = 4,
+    TwoPair = 3,
+    OnePair = 2,
+    HighCard = 1,
 }
 
-impl Card {
-    pub fn strength(&self) -> usize {
-        *self as usize
+#[derive(Debug, Eq)]
+struct Hand {
+    cards: [Card; 5],
+    bid: usize,
+    hand_type: HandType,
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // If the hand types are different, that's all that matters and we
+        // return their ordering
+        let hand_type_cmp = self.hand_type.cmp(&other.hand_type);
+        if hand_type_cmp != Ordering::Equal {
+            return hand_type_cmp;
+        }
+        // Else we have to compare the cards in each position in each hand
+        // until we find a pair that is different
+        for (a, b) in self.cards.iter().zip(&other.cards) {
+            let card_cmp = a.cmp(b);
+            if card_cmp == Ordering::Equal {
+                continue;
+            }
+            return card_cmp;
+        }
+        return Ordering::Equal;
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.cards == other.cards
+    }
+}
+
+impl TryFrom<&[Card]> for HandType {
+    type Error = String;
+
+    fn try_from(cards: &[Card]) -> Result<Self, Self::Error> {
+        if cards.len() != 5 {
+            return Err(format!("Cannot get HandType for {} cards", cards.len()));
+        }
+        let card_counts = cards
+            .iter()
+            .fold(HashMap::new(), |mut acc, c| {
+                acc.entry(c).and_modify(|count| *count += 1).or_insert(1);
+                acc
+            })
+            .into_values()
+            .collect::<Vec<_>>();
+        if card_counts.iter().any(|c| *c == 5) {
+            return Ok(HandType::FiveOfAKind);
+        }
+        if card_counts.iter().any(|c| *c == 4) {
+            return Ok(HandType::FourOfAKind);
+        }
+        let has_pair = card_counts.iter().any(|c| *c == 2);
+        if card_counts.iter().any(|c| *c == 3) {
+            if has_pair {
+                return Ok(HandType::FullHouse);
+            }
+            return Ok(HandType::ThreeOfAKind);
+        }
+        if has_pair {
+            if card_counts.iter().filter(|c| **c == 2).count() == 2 {
+                return Ok(Self::TwoPair);
+            }
+            return Ok(Self::OnePair);
+        }
+        return Ok(Self::HighCard);
     }
 }
 
@@ -53,9 +133,11 @@ impl TryFrom<(Vec<Card>, usize)> for Hand {
         if cards.len() != 5 {
             return Err(format!("Cannot build a hand with {} cards", cards.len()));
         }
+        let hand_type = HandType::try_from(cards.as_slice())?;
         Ok(Hand {
-            cards: (cards[0], cards[1], cards[2], cards[3], cards[4]),
+            cards: [cards[0], cards[1], cards[2], cards[3], cards[4]],
             bid,
+            hand_type,
         })
     }
 }
