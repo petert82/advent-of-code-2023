ed25681cbf60fda442dc80a6716ed15e98ceafab
Peter Thompson <peter@petethompson.net>
2023-12-12 21:41:24 +0100

    Add day 10 input parsing

--- src/day10.rs
+++ src/day10.rs
@@ -1,7 +1,179 @@
-use anyhow::Result;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+};
 
-pub fn part1(_input: &str) -> Result<usize> {
+use anyhow::{bail, Result};
+
+pub fn part1(input: &str) -> Result<usize> {
+    let grid = parse_grid(input)?;
+    println!("{}", grid);
     Ok(1)
+}
+
+struct Grid {
+    start: Coord,
+    w: usize,
+    h: usize,
+    pipes: HashMap<Coord, Pipe>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Coord(i32, i32);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Pipe {
+    NS,
+    EW,
+    NE,
+    NW,
+    SW,
+    SE,
+}
+
+impl Coord {
+    /// Is the coordinate in the grid?
+    pub fn is_valid(&self) -> bool {
+        self.0 >= 0 && self.1 >= 0
+    }
+
+    /// Gets valid neighbouring coordinates
+    pub fn neighbours(&self) -> Vec<Coord> {
+        vec![
+            Coord(self.0, self.1 - 1),
+            Coord(self.0, self.1 + 1),
+            Coord(self.0 - 1, self.1),
+            Coord(self.0 + 1, self.1),
+        ]
+        .into_iter()
+        .filter(Self::is_valid)
+        .collect()
+    }
+}
+
+impl Pipe {
+    /// For a Pipe at `pipe_pos`, gets the coordinates of the valid exits
+    /// i.e. exits that are not out of bounds of the grid
+    pub fn exits(&self, pipe_pos: Coord) -> Vec<Coord> {
+        let deltas = match self {
+            Self::NS => vec![(0, -1), (0, 1)],
+            Self::EW => vec![(1, 0), (-1, 0)],
+            Self::NE => vec![(0, -1), (1, 0)],
+            Self::NW => vec![(0, -1), (-1, 0)],
+            Self::SW => vec![(0, 1), (-1, 0)],
+            Self::SE => vec![(0, 1), (1, 0)],
+        };
+        deltas
+            .iter()
+            .map(|(dx, dy)| Coord(pipe_pos.0 + dx, pipe_pos.1 + dy))
+            .filter(|c| c.is_valid())
+            .collect()
+    }
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}x{} grid. Start is at {}\n\n",
+            self.w, self.h, self.start
+        )?;
+        for y in 0..self.h {
+            for x in 0..self.w {
+                match self.pipes.get(&Coord(x as i32, y as i32)) {
+                    None => write!(f, ".")?,
+                    Some(pipe) => write!(f, "{}", pipe)?,
+                }
+            }
+            write!(f, "\n")?;
+        }
+        write!(f, "\n")?;
+        Ok(())
+    }
+}
+
+impl Display for Coord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.0, self.1)
+    }
+}
+
+impl Display for Pipe {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let char = match self {
+            Self::NS => '║',
+            Self::EW => '═',
+            Self::NE => '╚',
+            Self::NW => '╝',
+            Self::SW => '╗',
+            Self::SE => '╔',
+        };
+        write!(f, "{}", char)
+    }
+}
+
+fn parse_grid(input: &str) -> Result<Grid> {
+    let mut start = None;
+    let mut pipes = HashMap::new();
+    let mut w = 0;
+    let mut h = 0;
+    for (y, line) in input.lines().enumerate() {
+        h += 1;
+        if w == 0 {
+            w = line.chars().count()
+        }
+        for (x, char) in line.chars().enumerate() {
+            let mut pipe = None;
+            match char {
+                'S' => start = Some(Coord(x as i32, y as i32)),
+                '|' => pipe = Some(Pipe::NS),
+                '-' => pipe = Some(Pipe::EW),
+                'L' => pipe = Some(Pipe::NE),
+                'J' => pipe = Some(Pipe::NW),
+                '7' => pipe = Some(Pipe::SW),
+                'F' => pipe = Some(Pipe::SE),
+                '.' => {}
+                _ => bail!("unexpected character {}", char),
+            }
+            if pipe.is_some() {
+                pipes.insert(Coord(x as i32, y as i32), pipe.take().unwrap());
+            }
+        }
+    }
+
+    let Some(start) = start else {
+        bail!("no start position found in input")
+    };
+
+    // Work out which kind of pipe is at the start
+    let start_connecting_coords = start
+        .neighbours()
+        .into_iter()
+        .map(|c| (c, pipes.get(&c)))
+        .filter(|(c, p)| {
+            if let Some(p) = p {
+                return p.exits(*c).iter().any(|e| *e == start);
+            }
+            false
+        })
+        .map(|(c, _p)| c)
+        .collect::<HashSet<Coord>>();
+    let start_pipe = vec![Pipe::NS, Pipe::EW, Pipe::NE, Pipe::NW, Pipe::SE, Pipe::SW]
+        .into_iter()
+        .filter(|p| {
+            let exits = p.exits(start);
+            if exits.len() != 2 {
+                return false;
+            }
+            exits
+                .into_iter()
+                .all(|e| start_connecting_coords.contains(&e))
+        })
+        .next()
+        .expect("expected to have a start pipe");
+    pipes.insert(start, start_pipe);
+
+    Ok(Grid { start, w, h, pipes })
 }
 
 #[cfg(test)]
