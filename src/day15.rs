c5ac46d7d7d88599da89ee3848e302bdf95a7173
Peter Thompson <peter@petethompson.net>
2023-12-27 14:45:15 +0100

    Update parsing for day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,10 +1,15 @@
 use anyhow::Result;
 use nom::{
-    bytes::complete::is_not, character::complete::char, combinator::map, multi::separated_list1,
+    branch::alt,
+    bytes::complete::is_not,
+    character::complete::{alpha1, char},
+    combinator::{map, map_parser},
+    multi::separated_list1,
+    sequence::{separated_pair, terminated},
     IResult,
 };
 
-use crate::parse::parse_all_to;
+use crate::parse::{number, parse_all_to};
 
 pub fn part1(input: &str) -> Result<usize> {
     let steps = parse_all_to(input, parse_initialization_sequence)?;
@@ -14,23 +19,56 @@ pub fn part1(input: &str) -> Result<usize> {
 
 #[derive(Debug)]
 struct Step {
-    value: String,
+    hash: usize,
+    instruction: Instruction,
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Insert { label: String, focal_length: usize },
+    Remove { label: String },
 }
 
 impl Step {
     pub fn hash(&self) -> usize {
-        self.value.chars().fold(0, |acc, c| {
-            let val = c as usize;
-            ((acc + val) * 17) % 256
-        })
+        self.hash
     }
 }
 
+fn hash(input: &str) -> usize {
+    input.chars().fold(0, |acc, c| {
+        let val = c as usize;
+        ((acc + val) * 17) % 256
+    })
+}
+
 fn parse_initialization_sequence(input: &str) -> IResult<&str, Vec<Step>> {
-    let step = map(is_not(","), |s| Step {
-        value: String::from(s),
-    });
+    let step = map_parser(is_not(","), parse_step);
     separated_list1(char(','), step)(input)
+}
+
+fn parse_step(input: &str) -> IResult<&str, Step> {
+    let hash = hash(input);
+    map(parse_instruction, move |instruction| Step {
+        hash,
+        instruction,
+    })(input)
+}
+
+fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
+    // e.g. "rn=1"
+    let insert = map(
+        separated_pair(alpha1, char('='), number),
+        |(label, focal_length)| Instruction::Insert {
+            label: String::from(label),
+            focal_length,
+        },
+    );
+    // e.g "cm-"
+    let remove = map(terminated(alpha1, char('-')), |label| Instruction::Remove {
+        label: String::from(label),
+    });
+    alt((insert, remove))(input)
 }
 
 #[cfg(test)]
